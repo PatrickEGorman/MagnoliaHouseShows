f905efd6e3f52daba7e7ad410fee3554302158e5
PatrickEGorman <gormanpe@vcu.edu>
2019-07-09 07:08:06 -0400

    Minor change

--- media/models.py
+++ media/models.py
@@ -5,10 +5,9 @@ from shows.models import Show
 
 class Flier(models.Model):
     image = models.ImageField()
+    date = models.DateField(default=None)
     caption = models.TextField(default='', blank=True)
     show = models.ForeignKey(Show, null=True, blank=True, related_name="fliers", on_delete=models.SET_NULL)
-    date = models.DateField(default=show.date)
-
 
     def __str__(self):
         if self.show:
@@ -23,11 +22,10 @@ class Flier(models.Model):
 
 class Photo(models.Model):
     image = models.ImageField()
+    date = models.DateField(blank=True, null=True, default=None)
     caption = models.TextField(default='', blank=True)
     artist = models.ForeignKey(Artist, null=True, blank=True, related_name="photos", on_delete=models.SET_NULL)
     show = models.ForeignKey(Show, null=True, blank=True, related_name="photos", on_delete=models.SET_NULL)
-    date = models.DateField(default=show.date)
-
 
     def __str__(self):
         if self.artist:
@@ -42,10 +40,10 @@ class Photo(models.Model):
 
 class YoutubeVideo(models.Model):
     youtube_url = models.URLField()
+    date = models.DateField(blank=True, null=True, default=None)
     caption = models.TextField(default='', blank=True)
     artist = models.ForeignKey(Artist, null=True, related_name='videos', on_delete=models.SET_NULL, blank=True)
     show = models.ForeignKey(Show, null=True, blank=True, related_name="videos", on_delete=models.SET_NULL)
-    date = models.DateField(default=show.date)
 
     def __str__(self):
         if self.artist:
