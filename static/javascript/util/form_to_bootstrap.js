50c95a80335b1db1d103c1fc0c8de71ecd2505af
PatrickEGorman <gormanpe@vcu.edu>
2019-07-31 21:14:00 -0400

    Created form for history page

---
+++ static/javascript/util/form_to_bootstrap.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'
+
+$(document).ready(function(){
+    $("p").addClass("form-group");
+    $("input").addClass("form-control");
+    $("form > ul").addClass("list-group");
+    $("ul.list-group > li").addClass("list-group-item");
+});
