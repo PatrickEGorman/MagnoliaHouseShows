f5ded20a9b062c77a9e8de862b661cee2ce5d595
PatrickEGorman <gormanpe@vcu.edu>
2019-08-11 21:30:05 -0400

    Added canceled to admin display

--- shows/admin.py
+++ shows/admin.py
@@ -5,9 +5,9 @@ from .models import Show
 
 
 class ShowAdmin(ModelAdminWithMeta):
-    list_display = ('date', 'priority', 'display_artists', 'display_genre', 'facebook', meta_created_date,
+    list_display = ('date', 'cancelled', 'priority', 'display_artists', 'display_genre', 'facebook', meta_created_date,
                     meta_created_by, meta_updated_by, meta_updated_date)
-    list_filter = ['date', 'priority', 'artists']
+    list_filter = ['date', 'priority', 'artists', 'cancelled']
     fields = [('date', 'time'), 'artists', 'priority', 'description', ('suggested_donation', 'suggested_donation_max'),
               ('facebook', 'instagram'), 'cancelled']
     ordering = ['-date']
